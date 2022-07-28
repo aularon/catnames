cbe7d1639f14343c66b93a1679f9589cd5c2dfc2
Hasan Arous <arous.hasan@gmail.com>
2022-07-29 01:14:42 +0300

    add ga!

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -23,6 +23,21 @@ export default function App(props: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Amiri&family=Lalezar&family=Noto+Nastaliq+Urdu&family=Vibes&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MLD5PN0JR8"
+        >
+        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MLD5PN0JR8');`,
+          }}
+        >
+        </script>
       </Head>
       <props.Component />
     </>
